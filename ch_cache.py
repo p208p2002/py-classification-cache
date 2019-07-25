707de479da5d48699b136eed163959512774c246
Philip <p208p2002@gmail.com>
2019-07-25 04:44:01 +0000

    no message

--- ch_cache.py
+++ ch_cache.py
@@ -6,28 +6,55 @@ import os
 with open('splitdata/seg_lists_0.pkl','rb') as f:
     data = pickle.load(f)
 
-CACHE_DIR = './.ch_cache'
+class ch_cache():
+    def __init__(self,cache_dir = './.ch_cache'):
+        self.CACHE_DIR = './.ch_cache'
+        self.__checkCacheDir()
     
-
-for d in data:
-    for word in d:
-        pass
-        parseResult = pinyin(word, style=Style.INITIALS, strict=False)
+    def __checkCacheDir(self):
+        if(not os.path.isdir(self.CACHE_DIR)):
+            os.mkdir(self.CACHE_DIR)
+            with open(self.CACHE_DIR+'/.gitignore', 'w', encoding='utf-8') as f:
+                f.write("*\n!.gitignore\n")
+    
+    def __parseHead(self,key):
+        parseResult = pinyin(key, style=Style.INITIALS, strict=False)
         parseResultHead = parseResult[0][0]
         parseResultHead = re.sub(r"[^A-Za-z|0-9]+", '', parseResultHead) #僅保留英文與數字
         if(len(parseResultHead)>1): #只取頭
             parseResultHead = parseResultHead[0]
         if(parseResultHead == ''):
-            parseResultHead = '._others'
+            parseResultHead = '_others'
         else:
             pass
-        
+        return parseResultHead
+    
+    def save(self, key, data):
+        CACHE_DIR = self.CACHE_DIR        
+        parseResultHead = self.__parseHead(key)
         # 檢查資料夾存在
-        if(not os.path.isdir(CACHE_DIR + parseResultHead)):
-            os.mkdir(CACHE_DIR + parseResultHead)
+        if(not os.path.isdir(CACHE_DIR + '/' + parseResultHead)):
+            os.mkdir(CACHE_DIR + '/' + parseResultHead)
         
         #
-        word = word + '.pkl'
-        with open(CACHE_DIR + parseResultHead + word , 'wb') as f:
-            pickle.dump(word,f)
-        
+        key = key + '.pkl'
+        with open(CACHE_DIR + '/' + parseResultHead + '/' + key , 'wb') as f:
+            pickle.dump(data,f)
+    
+    def get(self, key):
+        key = key + '.pkl'
+        subDir = self.__parseHead(key)
+        CACHE_DIR = self.CACHE_DIR
+        try:
+            with open(CACHE_DIR + '/' + subDir + '/' + key, 'rb') as f:
+                data = pickle.load(f)
+            return data
+        except:
+            return None
+
+
+if __name__ == "__main__":
+    ch = ch_cache()
+    ch.save("我們",[1,2,3])
+    a = ch.get("我們111")
+    print(a)
