deeace6e265480a09c19a8ee5f56e9011eb18bac
Philip <p208p2002@gmail.com>
2019-07-29 03:21:02 +0000

    no message

--- examples/main.py
+++
@@ -1,5 +0,0 @@
-from ..chcache import chcache
-chcache = chcache.chcache()
-chcache.save('菲力普',{'age':20,'money':5000,'en_name':'Philip'})
-Philip = chcache.get('菲力普')
-print(Philip)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="py-classification-cache",
-    version="0.0.2",
+    version="0.0.3",
     author="Philip Huang",
     author_email="p208p2002@gmail.com",
     description="Python classification cache",
@@ -10,6 +10,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/p208p2002/py-classification-cache",
     packages=setuptools.find_packages(),
+    install_requires=['pypinyin==0.35.4'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
